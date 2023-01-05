081116720c376d784c4f35fa30ecea3a4a3842e9
gdalsnes <gdalsnes@gmail.com>
2023-01-05 14:52:02 +0100

    example of crash with ENHANCE_YOUR_CALM

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+
+namespace GrpcClient
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { Credentials = ChannelCredentials.SecureSsl });
+
+
+			var client = new Greeter.GreeterClient(channel);
+
+			int i = 0;
+
+			while (true)
+			{
+				using (var streaming = client.SayHello())
+				{
+					await streaming.RequestStream.WriteAsync(new HelloRequest() { Name = "hello " + i++ });
+
+					while (await streaming.ResponseStream.MoveNext())
+					{
+						Console.WriteLine("Message from server: " + streaming.ResponseStream.Current.Message);
+					}
+
+					await streaming.RequestStream.CompleteAsync();
+				}
+			}
+		}
+	}
+}
